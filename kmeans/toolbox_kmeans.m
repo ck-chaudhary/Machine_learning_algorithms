af5231f5a801bfa8ac6d9b729aaabe36837b1f8d
ck-chaudhary <30978705+ck-chaudhary@users.noreply.github.com>
2019-02-08 04:04:17 +0530

    Create toolbox_kmeans.m

---
+++ kmeans/toolbox_kmeans.m
@@ -0,0 +1,11 @@
+close all;
+clear all;
+clc;
+features=4;
+filename='iris.data.txt';
+input=dlmread(filename);
+input=input(randperm(150),:);
+[idx,C,sumd,D]=kmeans(input(:,1:features),3)
+
+confusion = confusionmat(idx,input(:,features+1))
+accuracy=sum(max(confusion))*100/size(input,1)
